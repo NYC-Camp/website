88d36fed6155ca6a851da09b5105bcb1c8ca8020
Eric Sod <esod@northps.com>
2014-01-11 16:10:10 -0500

    update imagecache_actions

--- sites/all/modules/imagecache_actions/imagecache_actions.jquery.js
+++ sites/all/modules/imagecache_actions/imagecache_actions.jquery.js
@@ -1,25 +1,40 @@
 /* 
  * UI enhancements for the imagecache edit form
  */
+(function($){
 
-if(Drupal.jsEnabled){
-  $(document).ready(function(){
-    // canvasactions_roundedcorners
-    // check if independent corners are enabled and disable other fields
-    canvasactions_roundedcorners_form_disable_fields();
-    $(":checkbox#edit-data-independent-corners-set-independent-corners").change(function(){canvasactions_roundedcorners_form_disable_fields(); } );
-  });
-}
-
-
-function canvasactions_roundedcorners_form_disable_fields(){
-  // canvasactions_roundedcorners
-  // check if independent corners are enabled and disable other fields
-  if(!$(":checkbox#edit-data-independent-corners-set-independent-corners").attr("checked")){
-    $("#independent-corners-set-wrapper .form-text").attr("disabled", true).addClass("disabled");
-    $(":input#edit-data-radius").attr("disabled", false).removeClass("disabled");
-  } else {
-    $(":input#edit-data-radius").attr("disabled", true).addClass("disabled");
-    $("#independent-corners-set-wrapper .form-text").attr("disabled", false).removeClass("disabled");
+  /**
+   * Check if independent corners are enabled and disable other fields in the UI
+   */
+  Drupal.canvasactions_roundedcorners_form_disable_fields = function () {
+    // To get the right effect, we have to set the 'disabled' attribute on the 
+    // field, but set the class on the container item. Tedious.
+    if (!$(":checkbox#edit-data-independent-corners-set-independent-corners").attr("checked")){
+      $(".form-item-data-radius").removeClass("form-disabled");
+      $(".form-item-data-radius input").attr("disabled", false);
+      $("#independent-corners-set .form-item").addClass("form-disabled");
+      $("#independent-corners-set input").attr("disabled", true);
+    } 
+    else {
+      $(".form-item-data-radius").addClass("form-disabled");
+      $(".form-item-data-radius input").attr("disabled", true);
+      $("#independent-corners-set .form-item").removeClass("form-disabled");
+      $("#independent-corners-set input").attr("disabled", false);
+    }
   }
-}
+  /**
+   * Trigger the update when the form is ready, and add listener to the checkbox
+   */
+  Drupal.behaviors.canvasactions_roundedcorners = {
+    attach: function (context, settings) {
+      Drupal.canvasactions_roundedcorners_form_disable_fields();
+      $(":checkbox#edit-data-independent-corners-set-independent-corners").change(
+        function() {
+          Drupal.canvasactions_roundedcorners_form_disable_fields(); 
+        }
+      );
+    }
+  }
+
+})(jQuery);
+
