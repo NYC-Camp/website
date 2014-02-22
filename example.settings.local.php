831c322d58bec0efabd5d5e0033e6571fac50cb5
Elijah Lynn <elijah@elijahlynn.net>
2014-02-22 13:58:29 -0500

    Added an example.settings.local.php file.

---
+++ example.settings.local.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Rename this file to settings.local.php.
+ *
+ * The sites/default/settings.php includes this file by default if it exists.
+ */
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => '', // Your database name.
+  'username' => 'root', // Your MySQL username.
+  'password' => 'hitme', // Your MySQL password.
+  'host' => '127.0.0.1',
+);
